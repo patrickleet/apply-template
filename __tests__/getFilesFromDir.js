1307a2a2e43e4ff3f47085b7859c5d06da42aa67
Patrick Scott <patrick.scott@zx-ventures.com>
2017-04-24 15:26:00 -0400

    lib/getFilesFromDir and tests

---
+++ __tests__/getFilesFromDir.js
@@ -0,0 +1,13 @@
+const co = require('co'),
+      getFilesFromDir = require('getFilesFromDir');
+
+describe('when using getFilesFromDirectory on a flat directory', () => {
+  const testDir = './example-template/';
+
+  it('it returns the filenames inside of that directory, as well as the files from the subdirectories as an array', co.wrap(function *() {
+    let fileNames = yield getFilesFromDir(testDir);
+    expect(fileNames.length).toBe(2)
+    expect(fileNames[0]).toContain('index.js')
+    expect(fileNames[1]).toContain('bin-file')
+  }));
+});
